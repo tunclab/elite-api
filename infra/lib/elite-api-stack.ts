63ef95a9280e2b81ce442fc3cb92ccca3704d2a2
Kasun Dilunika <kasun@tunclab.com>
2022-11-27 22:10:08 +1300

    feat: domain name is linked to api endpoint.

--- infra/lib/elite-api-stack.ts
+++ infra/lib/elite-api-stack.ts
@@ -3,6 +3,9 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as agw from 'aws-cdk-lib/aws-apigateway';
 import * as logs from 'aws-cdk-lib/aws-logs';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as r53 from 'aws-cdk-lib/aws-route53';
+import * as r53t from 'aws-cdk-lib/aws-route53-targets';
 import * as path from 'path';
 
 export class EliteApiStack extends core.Stack {
@@ -27,13 +30,42 @@ export class EliteApiStack extends core.Stack {
       timeout: core.Duration.seconds(60),
     });
 
+    const hostedZone = r53.PublicHostedZone.fromHostedZoneAttributes(
+      this,
+      'hosted-zone',
+      {
+        hostedZoneId: 'Z02844713KMXUABLS9DJH',
+        zoneName: 'clearsigma.com',
+      },
+    );
+
+    const certificate = new acm.DnsValidatedCertificate(
+      this,
+      'elite-domain-cert',
+      {
+        domainName: 'api.elite.clearsigma.com',
+        hostedZone,
+      },
+    );
+
     const api = new agw.LambdaRestApi(this, 'elite-api', {
       restApiName: 'elite-api',
       handler: fn,
+      domainName: {
+        domainName: 'api.elite.clearsigma.com',
+        certificate,
+      },
+    });
+
+    const aRecord = new r53.ARecord(this, 'elite-api-a-record', {
+      zone: hostedZone,
+      recordName: 'api.elite',
+      target: r53.RecordTarget.fromAlias(new r53t.ApiGateway(api)),
     });
 
     this.exportValue(fn.functionArn, { name: 'api-fn-arn' });
     this.exportValue(api.restApiId, { name: 'api-id' });
     this.exportValue(fn.logGroup.logGroupName, { name: 'api-fn-logs' });
+    this.exportValue(aRecord.domainName, { name: 'api-a-record' });
   }
 }
